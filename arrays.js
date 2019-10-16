66383141abc1447ed097456d1ba407b749d81ee0
Jake Rose <jake1020@mac.com>
2019-10-16 02:00:51 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,23 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element){
+  return [element, ..array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  var unshift = array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  var push = array.push(element)
+  return array
+}
+
+function accessElementInArray() {
+
+}
